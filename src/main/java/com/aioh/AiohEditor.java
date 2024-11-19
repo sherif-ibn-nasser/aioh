5a40b105f6800addab73787146f5ca8960d41aa2
sherif2003 <sherif2102003@gmail.com>
2024-11-19 23:02:40 +0200

    Refactor

--- src/main/java/com/aioh/AiohEditor.java
+++ src/main/java/com/aioh/AiohEditor.java
@@ -2,11 +2,12 @@ package com.aioh;
 
 import com.aioh.graphics.AiohRenderer;
 import com.aioh.graphics.Timer;
-import glm_.vec2.Vec2;
 import glm_.vec4.Vec4;
 import org.lwjgl.opengl.GL;
 
-public class AiohEditor {
+import static org.lwjgl.glfw.GLFW.*;
+
+public class AiohEditor implements AiohWindow.EventsHandler {
     public static final int FONT_SIZE = 32;
     public static final int CURSOR_BLINK_THRESHOLD = 500;
     public static final int CURSOR_BLINK_PERIOD = 1000;
@@ -14,8 +15,7 @@ public class AiohEditor {
     private Timer timer = new Timer();
     private AiohRenderer renderer = new AiohRenderer();
     private StringBuilder text = new StringBuilder("");
-    private Vec2 cursorPos = new Vec2();
-    private float windowWidth, windowHeight;
+    private float cursorLine = 0, cursorCol = 0, windowWidth, windowHeight;
 
     public static boolean isDefaultContext() {
         return GL.getCapabilities().OpenGL32;
@@ -28,7 +28,12 @@ public class AiohEditor {
     public void loop(float windowWidth, float windowHeight) {
         this.windowWidth = windowWidth;
         this.windowHeight = windowHeight;
-        renderer.getFont().drawText(renderer, text, windowWidth / 2, windowHeight / 2 - cursorPos.getY() * renderer.getFont().getFontHeight());
+        renderer.getFont().drawText(
+                renderer,
+                text,
+                windowWidth / 2,
+                windowHeight / 2 + cursorLine * renderer.getFont().getFontHeight()
+        );
         drawCursor();
     }
 
@@ -39,7 +44,7 @@ public class AiohEditor {
             renderer.getFont().drawText(
                     renderer,
                     "|",
-                    cursorPos.getX() * FONT_SIZE / 2 - (float) FONT_SIZE / 4 + windowWidth / 2,
+                    cursorCol * FONT_SIZE / 2 - (float) FONT_SIZE / 4 + windowWidth / 2,
                     windowHeight / 2,
                     new Vec4((float) 0x4C / 256, (float) 0xAF / 256, (float) 0x50 / 256, 1)
             );
@@ -49,11 +54,74 @@ public class AiohEditor {
 
     }
 
-    public StringBuilder getText() {
-        return text;
+    @Override
+    public void onTextInput(char[] newChars) {
+        text.append(newChars);
+        System.out.println("Text buffer: " + text);
+
+        if (newChars[0] == '\n') {
+            cursorCol = 0;
+            cursorLine += 1;
+        } else
+            cursorCol += 1;
     }
 
-    public Vec2 getCursorPos() {
-        return cursorPos;
+    @Override
+    public void onKeyPressed(int keyCode) {
+        switch (keyCode) {
+            case GLFW_KEY_ENTER -> onTextInput(new char[]{'\n'});
+            case GLFW_KEY_BACKSPACE -> onBackspacePressed();
+            case GLFW_KEY_UP -> onUpArrowPressed();
+            case GLFW_KEY_DOWN -> onDownArrowPressed();
+            case GLFW_KEY_LEFT -> onLeftArrowPressed();
+            case GLFW_KEY_RIGHT -> onRightArrowPressed();
+        }
+    }
+
+    private void onBackspacePressed() {
+
+        if (text.isEmpty())
+            return;
+
+        var lastIdx = text.length() - 1;
+        var lastChar = text.charAt(lastIdx);
+        text.deleteCharAt(lastIdx);
+
+        if (lastChar == '\n') {
+            var lineLen = 0;
+            for (int i = text.length() - 1; i >= 0; i--) {
+                if (text.charAt(i) == '\n')
+                    break;
+                lineLen += 1;
+            }
+            cursorCol = lineLen;
+            cursorLine -= 1;
+        } else
+            cursorCol -= 1;
+
+        if (text.isEmpty())
+            return;
+
+        lastIdx = text.length() - 1;
+        if (!Character.isDefined(text.charAt(lastIdx)))
+            text.deleteCharAt(lastIdx);
+
+        System.out.println("Text buffer: " + text);
+    }
+
+    private void onUpArrowPressed() {
+
+    }
+
+    private void onDownArrowPressed() {
+
+    }
+
+    private void onLeftArrowPressed() {
+
+    }
+
+    private void onRightArrowPressed() {
+
     }
 }
