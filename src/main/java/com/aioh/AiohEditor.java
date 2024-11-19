2e07a9c46e4f67b462b50a5f65ce2749db9f4c46
sherif2003 <sherif2102003@gmail.com>
2024-11-19 23:41:30 +0200

    Store lines buffer in the editor

--- src/main/java/com/aioh/AiohEditor.java
+++ src/main/java/com/aioh/AiohEditor.java
@@ -5,36 +5,56 @@ import com.aioh.graphics.Timer;
 import glm_.vec4.Vec4;
 import org.lwjgl.opengl.GL;
 
+import java.util.ArrayList;
+
 import static org.lwjgl.glfw.GLFW.*;
 
 public class AiohEditor implements AiohWindow.EventsHandler {
     public static final int FONT_SIZE = 32;
     public static final int CURSOR_BLINK_THRESHOLD = 500;
     public static final int CURSOR_BLINK_PERIOD = 1000;
+    public static final int LINE_INITIAL_CAP = 512;
 
     private Timer timer = new Timer();
     private AiohRenderer renderer = new AiohRenderer();
-    private StringBuilder text = new StringBuilder("");
-    private float cursorLine = 0, cursorCol = 0, windowWidth, windowHeight;
+    private ArrayList<StringBuilder> lines = new ArrayList<>(32);
+    private float windowWidth, windowHeight;
+    private int cursorLine = 0, cursorCol = 0;
 
     public static boolean isDefaultContext() {
         return GL.getCapabilities().OpenGL32;
     }
 
     public void init() {
+        lines.add(new StringBuilder(LINE_INITIAL_CAP));
         renderer.init();
     }
 
     public void loop(float windowWidth, float windowHeight) {
         this.windowWidth = windowWidth;
         this.windowHeight = windowHeight;
+
+        renderer.begin();
+        drawText();
+        drawCursor();
+        renderer.end();
+    }
+
+    private void drawText() {
+        // TODO: Optimize and render only visible lines
+        var text = new StringBuilder(lines.size());
+        for (int i = 0; i < lines.size(); i++) {
+            text.append(lines.get(i));
+            if (i < lines.size() - 1)
+                text.append('\n');
+        }
+
         renderer.getFont().drawText(
                 renderer,
                 text,
                 windowWidth / 2,
                 windowHeight / 2 + cursorLine * renderer.getFont().getFontHeight()
         );
-        drawCursor();
     }
 
     private void drawCursor() {
@@ -44,7 +64,7 @@ public class AiohEditor implements AiohWindow.EventsHandler {
             renderer.getFont().drawText(
                     renderer,
                     "|",
-                    cursorCol * FONT_SIZE / 2 - (float) FONT_SIZE / 4 + windowWidth / 2,
+                    (float) (cursorCol * FONT_SIZE) / 2 - (float) FONT_SIZE / 4 + windowWidth / 2,
                     windowHeight / 2,
                     new Vec4((float) 0x4C / 256, (float) 0xAF / 256, (float) 0x50 / 256, 1)
             );
@@ -54,16 +74,39 @@ public class AiohEditor implements AiohWindow.EventsHandler {
 
     }
 
+    private StringBuilder getCurrentLine() {
+        return lines.get(cursorLine);
+    }
+
+    private char getCurrentChar() {
+        var line = getCurrentLine();
+        if (line.isEmpty()) {
+            if (cursorLine > 0)
+                return '\n';
+            return '\0';
+        }
+        return getCurrentLine().charAt(cursorCol - 1);
+    }
+
+    private boolean isCursorAtStartOfFile() {
+        return cursorLine == 0 && cursorCol == 0;
+    }
+
+    private boolean isCursorAtEndOfFile() {
+        return cursorLine == lines.size() && cursorCol == getCurrentLine().length();
+    }
+
     @Override
     public void onTextInput(char[] newChars) {
-        text.append(newChars);
-        System.out.println("Text buffer: " + text);
 
         if (newChars[0] == '\n') {
+            lines.add(new StringBuilder(LINE_INITIAL_CAP));
             cursorCol = 0;
             cursorLine += 1;
-        } else
+        } else {
+            getCurrentLine().append(newChars);
             cursorCol += 1;
+        }
     }
 
     @Override
@@ -80,33 +123,19 @@ public class AiohEditor implements AiohWindow.EventsHandler {
 
     private void onBackspacePressed() {
 
-        if (text.isEmpty())
+        if (isCursorAtStartOfFile())
             return;
 
-        var lastIdx = text.length() - 1;
-        var lastChar = text.charAt(lastIdx);
-        text.deleteCharAt(lastIdx);
-
-        if (lastChar == '\n') {
-            var lineLen = 0;
-            for (int i = text.length() - 1; i >= 0; i--) {
-                if (text.charAt(i) == '\n')
-                    break;
-                lineLen += 1;
-            }
-            cursorCol = lineLen;
+        if (cursorCol == 0) {
             cursorLine -= 1;
-        } else
+            cursorCol = getCurrentLine().length();
+            lines.removeLast();
+        } else {
             cursorCol -= 1;
+            getCurrentLine().deleteCharAt(cursorCol);
+        }
 
-        if (text.isEmpty())
-            return;
-
-        lastIdx = text.length() - 1;
-        if (!Character.isDefined(text.charAt(lastIdx)))
-            text.deleteCharAt(lastIdx);
-
-        System.out.println("Text buffer: " + text);
+        // TODO: Handle deleting 3 and 4 bytes characters
     }
 
     private void onUpArrowPressed() {
